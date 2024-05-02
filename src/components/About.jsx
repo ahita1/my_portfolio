1af8da9c403ec634a25a4e9eccd5d93f1bc05f9a
ahita1 <ahitafani833@gmail.com>
2024-05-02 11:17:33 +0300

    Initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,8 +47,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
+        I'm a skilled Full-stack Website Developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like Vue.js , React, Node.js, Django and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
